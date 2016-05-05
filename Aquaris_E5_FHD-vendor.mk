b397f9842acfd62324479a4f36bbdfbf94db7b81
ElTifo <tifo.el@gmail.com>
2016-05-05 17:01:31 +0100

    Update and rename k1_turbo-vendor.mk to Aquaris_E5_FHD-vendor.mk

---
+++ Aquaris_E5_FHD-vendor.mk
@@ -0,0 +1,9 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/bq/Aquaris_E5_FHD/proprietary/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/bq/Aquaris_E5_FHD/proprietary/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/bq/Aquaris_E5_FHD/proprietary/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/bq/Aquaris_E5_FHD/proprietary/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/bq/Aquaris_E5_FHD/proprietary/vendor,system/vendor)
